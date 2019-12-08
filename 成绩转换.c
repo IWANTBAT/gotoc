f9344f10dd5675d2c6af0a5762547c6a2b691604
IWANTBAT <18729932977@163.com>
2019-12-08 22:15:19 +0800

    输入成绩，转换为相应的等级

---
+++ 成绩转换.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int grade;
+	scanf("%d",&grade);
+
+if (grade>=90&&grade<=100)
+         printf("A");
+         else
+if(grade>=80&&grade<90)
+              printf("B");
+else
+if(grade>=70&&grade<80)
+                   printf("C");
+    else               
+if(grade>=60&&grade<70)
+                        printf("D");
+             else      
+if(grade>=0&&grade<60)
+                        printf("E");
+else
+                        printf("Score is error!");
+    return 0;
+} 
