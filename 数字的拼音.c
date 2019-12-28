08573ddabf8a9bd0c7c3c29b77201eb685741392
IWANTBAT <18729932977@163.com>
2019-12-28 22:44:06 +0800

    输入一个数字，输出他的中文读法，用拼音表示

---
+++ 数字的拼音.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{char number[20][20] = { "ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu" };
+
+ char unit[20][20] = {"","","shi","bai","qian","wan","shi", "bai", "qian", "yi", "shi"};
+			//根据需要扩充
+ char str[100];	
+
+ int len,hnz,i;
+
+ scanf("%s",str);
+ len=strlen(str);
+	
+ hnz=0;	//每四位为一组，本组是否有非零数，0为没有
+
+  for(i=0;i<len;i++)
+	{int digit=str[i]-'0';
+	 if(digit==0) 
+		{if(hnz==1&&(len-i)%4==1)
+			printf("%s ",unit[len-i]);	
+		 if(i+1<len&&str[i+1]!='0')	
+			printf("%s ",number[digit]);
+		}
+	  else
+		{if(!(digit==1&&i==0&&(len-i)%4==2))	
+			printf("%s ",number[digit]);
+		 printf("%s ",unit[len-i]);	
+		 hnz=1;
+		}	
+	if((len-i)%4==1) hnz=0;
+	}
+ return 0;
+} 
