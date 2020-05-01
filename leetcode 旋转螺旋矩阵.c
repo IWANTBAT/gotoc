fe26b398e2dd46ff5a10b4f681c27826b91fcfc0
IWANTBAT <18729932977@163.com>
2020-05-01 22:41:46 +0800

    顺时针旋转螺旋矩阵

---
+++ leetcode 旋转螺旋矩阵.c
@@ -0,0 +1,21 @@
+﻿/**
+* Note: The returned array must be malloced, assume caller calls free().
+*/
+int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize){
+	if (matrixSize == 0) {
+		*returnSize = 0; return 0;
+	}
+	*returnSize = matrixSize * (*matrixColSize);
+	int* ans = (int*)malloc(sizeof(int)* (*returnSize));
+	int i = 0, j = -1, num = 0, level = 0;
+	while (num < *returnSize) {
+		while (j < (*matrixColSize) - 1 - level) ans[num++] = matrix[i][++j];
+		while (i < matrixSize - 1 - level) ans[num++] = matrix[++i][j];
+		if (num == *returnSize) break;
+		while (j > level) ans[num++] = matrix[i][--j];
+		level++;
+		while (i > level) ans[num++] = matrix[--i][j];
+	}
+	return ans;
+}
+
