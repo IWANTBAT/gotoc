4f00f65b037426c327785311685fb2b6e763f7c7
IWANTBAT <18729932977@163.com>
2020-05-16 22:54:14 +0800

    假设按照升序排序的数组在预先未知的某个点上进行了旋转。
    ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
    
    请找出其中最小的元素。
    
    你可以假设数组中不存在重复元素
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

---
+++ leetcode  寻找旋转排序数组中的最小值.c
@@ -0,0 +1,14 @@
+﻿int findMin(int* nums, int numsSize){
+	if (numsSize == 1)return nums[0];
+	int left = 0, right = numsSize - 1;
+	while (right>left){
+		int mid = left + (right - left) / 2;
+		if (nums[mid]>nums[right]){   
+			left = mid + 1;     //右半边找
+		}
+		else{              //左半边找
+			right = mid;    
+		}
+	}
+	return nums[left];
+}
