c44f2b0b1de7b1fbdba577fecfed9e7aaeaa4c32
IWANTBAT <18729932977@163.com>
2020-05-19 00:21:21 +0800

    判断是否Wie单值二叉树

---
+++ leetcode  单值二叉树.c
@@ -0,0 +1,16 @@
+﻿bool Isvalid(struct TreeNode* root, int val){
+	if (root == NULL){
+		return true;
+	}
+	if (root->val != val){
+		return false;
+	}
+	return Isvalid(root->left, val) && Isvalid(root->right, val);
+}
+
+bool isUnivalTree(struct TreeNode* root){
+	if (root == NULL){
+		return true;
+	}
+	return Isvalid(root, root->val);
+}
