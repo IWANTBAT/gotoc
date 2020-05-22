d442eba1f26eb68f19fcc652b98b4c0d372f00dc
IWANTBAT <18729932977@163.com>
2020-05-22 22:51:02 +0800

    二分法寻找峰值

---
+++ leetcode  寻找峰值.c
@@ -0,0 +1,28 @@
+﻿int findPeakElement(int* nums, int numsSize){
+	int low = 0;
+	int high = numsSize - 1;
+	int mid = 0;
+
+	while (low + 1 < high)
+	{
+		mid = low + (high - low) / 2; // 向下取整
+
+		if (nums[mid] < nums[mid + 1])
+		{
+			low = mid;
+		}
+		else if (nums[mid] > nums[mid + 1])
+		{
+			high = mid;
+		}
+	}
+
+	if (nums[low] < nums[high])
+	{
+		return high;
+	}
+	else // (nums[low] > nums[high])
+	{
+		return low;
+	}
+}
