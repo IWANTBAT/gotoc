cbf542621554f4dd9fcf22d46371135d95ba3480
IWANTBAT <18729932977@163.com>
2020-05-23 23:51:12 +0800

    递归翻转二叉树

---
+++ leetcode  翻转二叉树.c
@@ -0,0 +1,15 @@
+﻿void inver(struct TreeNode* root){
+	if (root){
+		//交换交换
+		struct TreeNode* tem = root->left;
+		root->left = root->right;
+		root->right = tem;
+		inver(root->left);
+		inver(root->right);
+	}
+}
+
+struct TreeNode* invertTree(struct TreeNode* root){
+	inver(root);
+	return root;
+}
