714decbc7823b79d614b6464e8929701a41f9c21
IWANTBAT <18729932977@163.com>
2020-05-27 23:29:02 +0800

    通过找环来确定位置

---
+++ leetcode  寻找重复数.c
@@ -0,0 +1,22 @@
+﻿int findDuplicate(int* nums, int numsSize){
+	int fast, slow;
+
+	slow = 0;
+	fast = 0;
+
+	do {
+		slow = nums[slow];
+		fast = nums[nums[fast]];
+		//printf("%d, %d, ", slow, fast);
+	} while (slow != fast);
+
+	fast = 0;
+	do {
+		slow = nums[slow];
+		fast = nums[fast];
+	} while (slow != fast);
+
+	return slow;
+
+}
+
