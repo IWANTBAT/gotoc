e2586b253a4c3f45fcf11bbf5cc7e0ab88254111
IWANTBAT <18729932977@163.com>
2020-05-28 23:35:58 +0800

    第N个数

---
+++ leetcode  第N个数.c
@@ -0,0 +1,15 @@
+﻿class Solution {
+public:
+	int findNthDigit(int n) {
+		n -= 1;
+		for (long digits = 1; digits < 11; ++digits){
+			int first_num = pow(10, digits - 1);
+			if (n < 9 * first_num * digits){
+				return int(to_string(first_num + n / digits)[n%digits]) - '0';
+			}
+			n -= 9 * first_num * digits;
+		}
+		return 0;
+	}
+};
+
