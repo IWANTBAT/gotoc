eaf2a89af75e9a432ec4ac620391b7cb3f28ba99
IWANTBAT <18729932977@163.com>
2020-06-12 08:58:58 +0800

    判断树是否为平衡二叉树

---
+++ leetcode  平衡二叉树.c
@@ -0,0 +1,26 @@
+﻿/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     struct TreeNode *left;
+*     struct TreeNode *right;
+* };
+*/
+//分别求出左右子树的高度，判断相差的绝对值是否小于等于1
+int getH(struct TreeNode* root){
+	if (root == NULL){
+		return 0;
+	}
+	int l = getH(root->left);
+	int r = getH(root->right);
+	return l>r ? l + 1 : r + 1;
+}
+
+bool isBalanced(struct TreeNode* root){
+	if (root == NULL){
+		return true;
+	}
+	int l = getH(root->left);
+	int r = getH(root->right);
+	return abs(l - r) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+}
